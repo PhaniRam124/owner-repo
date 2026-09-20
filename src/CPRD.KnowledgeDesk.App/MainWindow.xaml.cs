9fa5e511daddd8f79de3b72c475cba3f34803e0d
C Phani Ram <phani@carecodepro.com>
2026-09-20 23:53:01 +0530

    ui: wire main window lifecycle

--- src/CPRD.KnowledgeDesk.App/MainWindow.xaml.cs
+++ src/CPRD.KnowledgeDesk.App/MainWindow.xaml.cs
@@ -1,8 +1,16 @@
 using System.Windows;
+using CPRD.KnowledgeDesk.App.ViewModels;
 
 namespace CPRD.KnowledgeDesk.App;
 
 public partial class MainWindow : Window
 {
-    public MainWindow() => InitializeComponent();
+    public MainWindow(MainWindowViewModel viewModel)
+    {
+        InitializeComponent();
+        DataContext = viewModel;
+    }
+
+    private void Exit_Click(object sender, RoutedEventArgs e) =>
+        Application.Current.Shutdown();
 }
